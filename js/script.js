f56e2ac339a203c7cbca9a428284d8290af5fa0e
Vlad <vladi28061750@gmail.com>
2020-11-25 22:05:46 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const obj = {
+//      name: "John",
+//      age: 25,
+//      isMarride: false
+// };
+
+// console.log(obj["age"]);
+
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
+// alert('hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+
+// const answer = +prompt('Вам есть 18?', '');
+// console.log(typeof(answer));
+
